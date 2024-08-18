7a9fb56fcdce01aa5f0d33498dd20ac8487fa078
roy collins nguithi <roynguithi@student.moringaschool.com>
2024-08-18 19:39:57 +0300

    react-hooks-react-router-code-along-v6

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes.js";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 
+import UserProfile from "./pages/UserProfile";
+
+import ErrorPage from "./pages/ErrorPage";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
+    element: <Home />,
+    errorElement: <ErrorPage />
   }, 
   {
     path: "/about",
-    element: <About />
+    element: <About />,
+    errorElement: <ErrorPage />
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/profile/:id",
+    element: <UserProfile />,
+    errorElement: <ErrorPage />
   }
-])
-
+]);
+  
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
 
